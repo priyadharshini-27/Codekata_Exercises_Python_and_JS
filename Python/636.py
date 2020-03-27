22d5858fb427148375a749b183f73c8042b808f6
priyadharshini-27 <61590245+priyadharshini-27@users.noreply.github.com>
2020-03-27 23:44:10 +0530

    Delete 636.py

--- Python/636.py
+++
@@ -1,9 +0,0 @@
-'''Jennyfer is fond of strings. 
-She wants to read the character from right to left, so she wants you to design a suitable algorithm which satisfy her desire.
-Sample Input :
-jennyfer
-Sample Output :
-Refynnej
-'''
-string=input()
-print((string[::-1]).capitalize())
