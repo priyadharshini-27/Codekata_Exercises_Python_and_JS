1647ec6d4a50db1b8bb33a8814980411fba04b3d
priyadharshini-27 <61590245+priyadharshini-27@users.noreply.github.com>
2020-03-27 23:28:35 +0530

    string length

---
+++ Python/strings/35.py
@@ -0,0 +1,12 @@
+'''Given a string S, find its length without using any pre-defined functions.
+Sample Testcase :
+INPUT
+codekata
+OUTPUT
+8
+'''
+s=input()
+c=0
+for letter in s:
+    c+=1
+print(c)
