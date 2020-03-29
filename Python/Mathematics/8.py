8e229f36e5c082e62cabaf0ac805bb785be0f150
priyadharshini-27 <61590245+priyadharshini-27@users.noreply.github.com>
2020-03-29 18:40:13 +0530

    round off to greater

---
+++ Python/Mathematics/8.py
@@ -0,0 +1,10 @@
+'''Given a floating point number with 1 decimal place round it off to nearest greater integer and print it.
+Sample Testcase :
+INPUT
+2.6
+OUTPUT
+3
+'''
+import math
+n=float(input())
+print(math.ceil(n))
