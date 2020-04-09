30e177c2f2a8213530a91406ebf54650665b7334
priyadharshini-27 <61590245+priyadharshini-27@users.noreply.github.com>
2020-04-09 11:55:49 +0530

    even factor does not exists for N print '-1'

---
+++ Python/Array/98.py
@@ -0,0 +1,19 @@
+'''
+Given a number N, print the even factors of N.
+If the even factor does not exists for N print '-1'.
+Input Size : 1 <= N <= 1000
+Sample Testcase :
+INPUT
+8
+OUTPUT
+2 4 8
+'''
+n=int(input())
+arr=[]
+for f in range(2,n+1):
+    if n%f==0 and f%2==0:
+        arr.append(f)
+if len(arr)!=0:
+    print(' '.join(map(str,arr)))
+else:
+    print(-1)
